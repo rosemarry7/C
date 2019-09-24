e159155d42124622f7db1e1fc745fbc2426fd991
windzzz <wind_9527@outlook.com>
2019-09-24 14:29:36 +0800

    update

---
+++ C Primer/4字符串和格式化输入输出/talkback.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#define DENSETY 62.4
+
+int main(){
+    float weight,volumn;
+    int size,letters;
+    char name[40];
+
+    printf("Hi what's your first name? \n");
+    scanf("%s",name);
+    printf("%s, what's your weight in pounds \n",name);
+    scanf("%f",&weight);
+    size = sizeof name;
+    letters = strlen(name);
+    volumn = weight/DENSETY;
+    printf("well %s your volumn is %2.2f cubic feet \n",name,volumn);
+    printf("also your first name has %d letters",letters);
+    printf("and we have %d bytes to store it \n",size);
+    return 0;
+}

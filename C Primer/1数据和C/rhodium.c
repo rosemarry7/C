1392c3d382a601e938e99f99218dba7858b793d2
windzzz <wind_9527@outlook.com>
2019-09-24 14:30:04 +0800

    update

--- C Primer/1数据和C/rhodium.c
+++ C Primer/1数据和C/rhodium.c
@@ -19,8 +19,10 @@
  * 
  * 
  * ****************************************************************/
-#include <stdio.h>
-int main(void) {
+
+/**
+ * includ <stdio.h>
+ * int main(void) {
     float weight;
     float value;
 
@@ -35,6 +37,49 @@ int main(void) {
     printf("eat more to maintain your weight \n");
     return 0;
 }
+**/
+/* #include <stdio.h>
+int main(void){
+    int ten = 10;
+    int two = 2;
+    printf("Doing it right.\n");
+    printf("%d minus %d is %d.\n", ten, 2,ten-two);
+    printf("doing it wrong.\n");
+    printf("%d minus %d is %d.\n",ten);
+    return 0;
+} */
 
+/********************************
+ * short int 或者简写为short类型，可能占用比int类型更少的存储空间，用于仅需小数值的场合以节省空间，它是一种有符号类型
+ * long int 与short int 相反
+ * long long int 需要更大数的场合
+ * unsigned int 用于非负值的场合
+ * C90 unsigned short int , unsigned long int 
+ * C99 unsigned long long int
+ * 
+ * *******************************/
+/* #include <stdio.h>
+int main(void){
+    int i = 2147483647;
+    unsigned int j = i*2+1;
+    printf("%d %d %d\n",i,i+1,i+2);
+    printf("%u %u %u\n",j,j+1,j+2);
+    int a;
+    scanf("%d",&a);
+    printf("%c",a);
+    return 0;
+} */
+#include <stdio.h>
+#include <string.h>
+#define DENSETY 62.4
 
+int main(){
+    float weight,volumn;
+    int size,letters;
+    char name[40];
+
+    printf("Hi what's your first name? \n");
+    scanf("%s",name);
+    
+}
 

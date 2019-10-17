496443a9e2d979f100c4041a09b35bb6c8d0b908
windzzz <wind_9527@outlook.com>
2019-10-17 18:09:33 +0800

    aaa

--- C Primer/10指针和数组/array.c
+++ C Primer/10指针和数组/array.c
@@ -10,13 +10,24 @@ int main(void){
      }
      return 0;
 } */
-#include <stdio.h>
+/*#include <stdio.h>
 #define MONTHS 12
 int main(){
     int days[MONTHS] = {30, 29, [4]=30, 30, 31, [1]=12};
     int i;
     for(i=0;i<MONTHS;i++){
         printf("%d",days[i]);
+    }
+    return 0;
+}*/
+#include <stdio.h>
+int main(void){
+    int i,j;
+    for(i=9;i>0;i--){
+        for(j=i;j>0;j--){
+            printf("%d*%d=%2d \t",i,j,i*j);
+        }
+        printf("\n");
     }
     return 0;
 }
---
+++ C Primer/10指针和数组/data.c
@@ -0,0 +1,9 @@
+//
+// Created by wind on 2019/10/17.
+//
+#include <stdio.h>
+int main(void){
+    printf("Hello Data!");
+    return 0;
+}
+

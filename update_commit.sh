c32e67b5ff698dd1afc77077a8fe42adfe350d51
rosemarry7 <rosemarry7@protonmail.com>
2019-10-21 10:37:28 +0800

    init

--- update_commit.sh
+++ update_commit.sh
@@ -1,17 +1,16 @@
-#! /bin/bash
-
+#!/bin/sh
 git filter-branch --env-filter '
 OLD_EMAIL="wind_9527@outlook.com"
 CORRECT_NAME="rosemarry7"
-CORRECT_EMAIL=rosemarry7@protonmail.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL"  ]
+CORRECT_EMAIL="rosemarry7@protonmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-        export GIT_COMMITTER_NAME="$CORRECT_NAME"
-            export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-        fi
-        if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL"  ]
-        then
-                export GIT_AUTHOR_NAME="$CORRECT_NAME"
-                    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-                fi
-                ' --tag-name-filter cat -- --branches --tags
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags

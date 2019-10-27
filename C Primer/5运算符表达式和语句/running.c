2cdb13798b6ebeb4467582ec2054fddaf49a22db
wind <wind@winddeMac-mini.local>
2019-10-27 21:36:39 +0800

    add

---
+++ C Primer/5运算符表达式和语句/running.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+const int S_PER_MIN = 60;
+const int S_PER_H=3600;
+const double M_PER_K=0.62137;
+
+int main(void){
+    double distk,distm;
+    double rate;
+    int min,sec;
+    int time;
+    double mtime;
+    int mmin,msec;
+    _Bool bool = 1;
+
+    return 0;
+}
